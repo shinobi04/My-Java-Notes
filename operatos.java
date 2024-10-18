ad1f93c8d940b18b155c19707cfcbffea5e3e624
Anurag Kumar Singh <88939568+shinobi04@users.noreply.github.com>
2024-10-19 00:19:11 +0530

    Update operatos.java

--- operatos.java
+++ operatos.java
@@ -7,7 +7,60 @@ public class operatos {
         - Example (arithmetic, relational ,logical ,bitwise etc)
          */
 
-        /* Arithmetic Operators */
+        /* Arithmetic Operators
+        Binary         Unary
+           +            ++
+           -            --
+           /
+           x
+           % -> Gives remainder
+         */
+        //Example
+        /*
+        int a =10;
+        int b= 20;
+        System.out.println("a+b = " + (a+b));
+        System.out.println("a-b = " + (a-b));
+        System.out.println("a/b = " + (a/b));
+        System.out.println("a*b = " + (a*b));
+        */
+        /*
+        Unary Operators
+        -If you want to increase/decrease value by 1
+        -for example a=a+1 or a=a-1
+        -you can use ++ or -- operators
+        -these are also known as increment/decrement operators
+        -Further they are of two types pre-increment/decrement and post
+        - ++a / a++
+        - pre-increment / post-increment
+        - in pre increment first value is changed and then used where as
+        - in post increment first it is used and then change
+        - here is an example of pre-increment
 
+        int a=10;
+        int b=++a;
+        System.out.println(a); //output 11
+        System.out.println(b); //output 11
+
+        first value of a is changed to 11 then it is assigned to b hence b is also 11
+
+        here is example of post increment
+
+        int a=10;
+        int b=a++;
+        System.out.println(a); //output 11
+        System.out.println(b); //output 10
+
+        at first value of a is 10 then it is assigned to b
+        after assignment it is incremented by 1 a++ hence value of a become 11
+        and updated in memory
+         */
+
+        //Relational Operators
+        /* There is not much i know it
+        - here are few example of relational ope
+        - == , != , <= , >= , < , >
+         */
+        
     }
 }
