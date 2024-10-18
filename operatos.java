0d111acde3f5a57ac5089e436182d7867f412d71
Anurag Kumar Singh <88939568+shinobi04@users.noreply.github.com>
2024-10-18 23:44:33 +0530

    Add files via upload

---
+++ operatos.java
@@ -0,0 +1,13 @@
+public class operatos {
+    public static void main(String[] args) {
+        /* OPERATORS in Java
+        -Symbols that tells compiler to perform some operation
+        - example +-/x
+        - There are Different types of operators in java
+        - Example (arithmetic, relational ,logical ,bitwise etc)
+         */
+
+        /* Arithmetic Operators */
+
+    }
+}
