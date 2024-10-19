e127180042991b4a86151407fb6a26929806800f
Anurag Kumar Singh <88939568+shinobi04@users.noreply.github.com>
2024-10-19 10:56:27 +0530

    Add files via upload

---
+++ Conditional_Statment.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Conditional_Statment {
+    public static void main(String[] args) {
+        /* if - else statement
+        syntax-:
+        if (condition){
+        }
+        else if {
+        }
+        else{
+        }
+        Example -:
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        if (age >=18){
+            System.out.println("You are adult");
+        }
+        else {
+            System.out.println("You are not an adult");
+        }
+
+        Ternary Operator
+        syntax
+        variable = condition ? statement1:statement2
+        if the condition is true statement1 is executed and if false
+        statement 2 is executed
+         */
+        Scanner sc = new Scanner(System.in);
+//        int number = sc.nextInt();
+//        String Type = ((number%2)==0)?"Even":"odd";
+//        System.out.println(Type);
+    }
+}
